a646bbedf5f42173fe1d74d53dc51e8d072ec7f9
zeew00kim <tukzeew00@gmail.com>
2025-09-09 16:23:34 +0900

    dev : add stack DFS logic

---
+++ data_struct/queueBFS.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+using namespace std;
+
+template <class T>
+class Queue {
+	int front, rear, size;
+	vector<int> data;
+public:
+	Queue(int size = 5) {
+		this->size = size;
+		front = rear = 0;
+		data.resize(this->size);
+	}
+	bool is_empty() {
+		return (front == rear) ? true : false;
+	}
+	bool is_full() {
+		return (front == (rear + 1) % size) ? true : false;
+	}
+	void enqueue(T element) {
+		if (is_full()) throw runtime_error("overflow error");
+		data[rear] = element;
+		rear = (rear + 1) % size;
+	}
+	T dequeue() {
+		if (is_empty()) throw runtime_error("underflow error");
+		T temp = data[front];
+		front = (front + 1) % size;
+		return temp;
+	}
+	T peek() {
+		if (is_empty()) throw runtime_error("underflow error");
+		return data[front];
+	}
+};
+
+vector<vector<int>> graph;
+vector<bool> visited;
+
+void queueBFS(const int& start, const int& size) {
+	Queue<int> queue(size);
+	queue.enqueue(start);
+	
+	while (!queue.is_empty()) {
+		int node = queue.dequeue();
+		if (!visited[node]) {
+			visited[node] = true;
+			cout << node << " ";
+		}
+		for (auto it = graph[node].begin(); it != graph[node].end(); ++it) {
+			if (!visited[*it]) queue.enqueue(*it);
+		}
+	}
+}
+
+int main() {
+
+	int size = 6;
+
+	graph.resize(size + 1);
+	visited.resize(size + 1, false);
+
+	graph[1] = { 2, 3 };
+	graph[2] = { 1, 4, 5 };
+	graph[3] = { 1 };
+	graph[4] = { 2 };
+	graph[5] = { 2, 6 };
+	graph[6] = { 5 };
+
+	queueBFS(1, size + 1);
+
+	return 0;
+}
